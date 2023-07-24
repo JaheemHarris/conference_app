1e1adb8a574387c84e0c41e60dadad2221533685
JaheemHarris <namtetsuya@gmail.com>
2023-07-24 10:57:21 +0300

    Last com

--- src/components/FormulaireConference.vue
+++ src/components/FormulaireConference.vue
@@ -3,12 +3,16 @@
     <form @submit.prevent="handleSubmit" class="col-lg-8 col-sm-10">
       <h2 class="text-center">Inscription pour #MaConf2020</h2>
       <!-- Affichage des erreurs -->
-      <div v-if="hasErrors()" class="errors row">
-        <div class="errors-box col-12">
-          <p>Corrigez les erreurs suivantes:</p>
-          <ul>
-            <li v-for="error in errors()" :key="error.field">{{ error.message }}</li>
-          </ul>
+      <div v-if="hasErrors()" class="errors">
+        <div class="errors-box">
+          <div class="title" style="background-color: rgb(207, 207, 207);">
+            <p style="font-size: 1rem;">Corrigez les erreurs suivantes:</p>
+          </div>
+          <div class="errors-content">
+            <ul>
+              <li v-for="error in errors()" :key="error.field">{{ error.message }}</li>
+            </ul>
+          </div>
         </div>
       </div>
       <div>
@@ -17,17 +21,17 @@
           <div class="row justify-content-between">
             <div class="col-5">
               <label class="form-label" for="prenom">Prénom*</label>
-              <input type="text" class="form-control" name="prenom" id="prenom" v-model="formData.prenom" />
+              <input type="text" class="form-control" name="prenom" id="prenom" v-model="formData.prenom" :disabled="formSubmitted && !hasErrors()"/>
             </div>
             <div class="col-5">
               <label class="form-label" for="nom">Nom*</label>
-              <input type="text" class="form-control" name="nom" id="nom" v-model="formData.nom" />
+              <input type="text" class="form-control" name="nom" id="nom" v-model="formData.nom" :disabled="formSubmitted && !hasErrors()"/>
             </div>
           </div>
           <div class="row">
             <div class="col-12">
               <label class="form-label" for="email">Adresse email*</label>
-              <input type="email" class="form-control" name="email" id="email" v-model="formData.email" />
+              <input type="email" class="form-control" name="email" id="email" v-model="formData.email" :disabled="formSubmitted && !hasErrors()"/>
             </div>
           </div>
           <div class="row">
@@ -35,11 +39,13 @@
               <label for="sexe-options" class="form-label">Sexe</label>
               <div id="sexe-options">
                 <div class="form-check form-check-inline">
-                  <input class="form-check-input" type="radio" name="sexe" id="inlineRadio1" value="H">
+                  <input class="form-check-input" type="radio" name="sexe" id="inlineRadio1" value="H"
+                    v-model="formData.sexe" :disabled="formSubmitted && !hasErrors()">
                   <label class="form-check-label" for="inlineRadio1">Je suis un homme</label>
                 </div>
                 <div class="form-check form-check-inline">
-                  <input class="form-check-input" type="radio" name="sexe" id="inlineRadio2" value="F">
+                  <input class="form-check-input" type="radio" name="sexe" id="inlineRadio2" value="F"
+                    v-model="formData.sexe" :disabled="formSubmitted && !hasErrors()">
                   <label class="form-check-label" for="inlineRadio2">Je suis une femme</label>
                 </div>
               </div>
@@ -49,39 +55,39 @@
             <div class="col-12">
               <label class="form-label" for="institution">Institution*</label>
               <input type="text" class="form-control" name="institution" id="institution"
-                v-model="formData.institution" />
+                v-model="formData.institution" :disabled="formSubmitted && !hasErrors()"/>
             </div>
           </div>
           <div class="row justify-content-between">
             <div class="col-5">
               <label class="form-label" for="adresse">Adresse*</label>
-              <input type="text" class="form-control" name="adresse" id="adresse" v-model="formData.adresse" />
+              <input type="text" class="form-control" name="adresse" id="adresse" v-model="formData.adresse" :disabled="formSubmitted && !hasErrors()"/>
             </div>
             <div class="col-5">
               <label class="form-label" for="pays">Pays*</label>
-              <input type="text" class="form-control" name="pays" id="pays" v-model="formData.pays" />
+              <input type="text" class="form-control" name="pays" id="pays" v-model="formData.pays" :disabled="formSubmitted && !hasErrors()"/>
             </div>
           </div>
           <div class="row justify-content-between">
             <div class="col-5">
               <label class="form-label" for="code_postal">Code Postal*</label>
-              <input type="text" class="form-control" name="code_postal" id="code_postal" v-model="formData.codePostal" />
+              <input type="text" class="form-control" name="code_postal" id="code_postal" v-model="formData.codePostal" :disabled="formSubmitted && !hasErrors()"/>
             </div>
             <div class="col-5">
               <label class="form-label" for="ville">Ville*</label>
-              <input type="text" class="form-control" name="ville" id="ville" v-model="formData.ville" />
+              <input type="text" class="form-control" name="ville" id="ville" v-model="formData.ville" :disabled="formSubmitted && !hasErrors()"/>
             </div>
           </div>
           <div class="row justify-content-between">
             <div class="col-5">
               <label class="form-label" for="page_web_personnelle">Page web personnelle</label>
               <input type="text" class="form-control" name="page_web_personnelle" id="page_web_personnelle"
-                v-model="formData.pageWebPersonnelle" />
+                v-model="formData.pageWebPersonnelle" :disabled="formSubmitted && !hasErrors()"/>
             </div>
             <div class="col-5">
               <label class="form-label" for="page_web_institution">Page web institution</label>
               <input type="text" class="form-control" name="page_web_institution" id="page_web_institution"
-                v-model="formData.pageWebInstitution" />
+                v-model="formData.pageWebInstitution" :disabled="formSubmitted && !hasErrors()"/>
             </div>
           </div>
         </div>
@@ -92,17 +98,17 @@
         <div class="row">
           <div class="form-check form-check-inline">
             <input class="form-check-input" type="radio" name="inscription" id="inlineRadio1" value="150"
-              v-model="formData.inscription" />
+              v-model="formData.inscription" :disabled="formSubmitted && !hasErrors()"/>
             <label class="form-check-label" for="inlineRadio1">Etudiant (150 EUR)</label>
           </div>
           <div class="form-check form-check-inline">
             <input class="form-check-input" type="radio" name="inscription" id="inlineRadio2" value="200"
-              v-model="formData.inscription" />
+              v-model="formData.inscription" :disabled="formSubmitted && !hasErrors()"/>
             <label class="form-check-label" for="inlineRadio2">Académique (200 EUR)</label>
           </div>
           <div class="form-check form-check-inline">
             <input class="form-check-input" type="radio" name="inscription" id="inlineRadio2" value="300"
-              v-model="formData.inscription" />
+              v-model="formData.inscription" :disabled="formSubmitted && !hasErrors()"/>
             <label class="form-check-label" for="inlineRadio2">Entreprise (300 EUR)</label>
           </div>
         </div>
@@ -113,12 +119,12 @@
         <div class="row">
           <div class="form-check form-check-inline">
             <input class="form-check-input" type="radio" name="hebergement" id="inlineRadio1" value="150"
-              v-model="formData.hebergement" />
+              v-model="formData.hebergement" :disabled="formSubmitted && !hasErrors()"/>
             <label class="form-check-label" for="inlineRadio1">Avec réservation (150 EUR)</label>
           </div>
           <div class="form-check form-check-inline">
             <input class="form-check-input" type="radio" name="hebergement" id="inlineRadio2" value="0"
-              v-model="formData.hebergement" />
+              v-model="formData.hebergement" :disabled="formSubmitted && !hasErrors()"/>
             <label class="form-check-label" for="inlineRadio2">Sans réservation (0 EUR)</label>
           </div>
         </div>
@@ -126,10 +132,9 @@
       <hr>
 
       <button type="submit" class="btn btn-primary">Pré-valider</button>
-    </form>
 
-    <!-- Affichage du récapitulatif de l'inscription -->
-    <RecapitulatifFormulaire v-if="showSummary" :formData="formData" />
+      <RecapitulatifFormulaire v-if="showSummary" :formData="formData" />
+    </form>
   </div>
 </template>
   
@@ -185,47 +190,15 @@ export default {
       },
     ];
 
-    const inscriptions = [
-      {
-        key: 1,
-        label: "Etudiant",
-        montant: 150
-      },
-      {
-        key: 2,
-        label: "Académique",
-        montant: 200
-      },
-      {
-        key: 3,
-        label: "Entreprise",
-        montant: 300
-      },
-    ];
-
-    const hebergements = [
-      {
-        key: 1,
-        label: 'Avec réservation',
-        montant: 150
-      },
-      {
-        key: 2,
-        label: 'Sans réservation',
-        montant: 0
-      },
-    ];
-
     return {
       showSummary: false,
       formSubmitted: false,
       errorMessages,
-      inscriptions,
-      hebergements,
       formData: {
         prenom: "",
         nom: "",
         email: "",
+        sexe: "",
         institution: "",
         codePostal: "",
         pays: "",
@@ -245,6 +218,7 @@ export default {
         .filter(({ field }) => !this.formData[field]);
     },
     handleSubmit() {
+      console.log(this.formData);
       this.formSubmitted = true;
       if (!this.hasErrors()) {
         this.showSummary = true;
@@ -258,8 +232,10 @@ export default {
 </script>
 
 <style scoped>
-  .errors-box{
-    border: 0.5px gray solid;
-    border-radius: 5px 5px;
-  }
+.errors-box {
+  margin-top: 2%;
+  margin-bottom: 2%;
+  border: 0.5px rgb(207, 207, 207) solid;
+  border-radius: 5px 5px;
+}
 </style>
--- src/components/RecapitulatifFormulaire.vue
+++ src/components/RecapitulatifFormulaire.vue
@@ -1,30 +1,106 @@
 <template>
-    <div>
-      <h2>Récapitulatif de l'inscription</h2>
-      <p><strong>Email :</strong> {{ formData.email }}</p>
-      <!-- Affichez ici les autres champs du formulaire -->
-  
-      <p><strong>Type d'inscription :</strong> {{ formData.typeOfRegistration }}</p>
-      <p><strong>Mode d'hébergement :</strong> {{ formData.accommodation }}</p>
-  
-      <p><strong>Montant total :</strong> {{ calculateTotalAmount }} €</p>
+  <div class="row summary">
+    <div class="title" style="background-color: rgb(207, 207, 207);">
+      <p style="font-size: 1rem;">Récapitulatif de l'inscription</p>
     </div>
-  </template>
+    <div class="content">
+      <p>Bonjour {{ formData.sexe === 'H' ? 'monsieur' : 'madame' }} {{ `${formData.prenom} ${formData.nom}` }}, vous avez
+        procédé à une inscription pour la conférence #MaConf2020.</p>
+      <!-- Affichez ici les autres champs du formulaire -->
+
+      <p>Le détail de votre enregistrement est le suivant:</p>
+      <ul>
+        <li>{{ getSelectedInscription.label }} {{ getSelectedInscription.montant }} EUR</li>
+        <li>{{ getSelectedHebergement.label }} {{ getSelectedHebergement.montant }} EUR</li>
+      </ul>
+
+      <p>Le montant total est de {{ calculateTotalAmount }} EUR</p>
+      <p>Un email vous sera envoyé prochainement à cette adresse {{ formData.emali }} pour la mise en paiement de votre
+        inscription. <br>
+        Merci de consulter votre messagerie et de procéder au règlement de votre inscription.
+      </p>
+      <p>En vous remerciant de votre inscription, à très bientôt à la conférence #MaConf2020.</p>
+      <div class="content-buttons">
+        <button class="btn btn-primary" type="button">Confirmer</button>
+        <button class="btn btn-primary" type="button">Modifier les données</button>
+      </div>
+    </div>
+  </div>
+</template>
   
-  <script>
-  export default {
-    name: 'RecapitulatifFormulaire',
-    props: {
-      formData: {
-        type: Object,
-        required: true,
-      },
+<script>
+export default {
+  name: 'RecapitulatifFormulaire',
+  props: {
+    formData: {
+      type: Object,
+      required: true,
     },
-    computed: {
-      calculateTotalAmount() {
-        return formData.inscription + formData.hebergement;
+  },
+  data() {
+    const inscriptions = [
+      {
+        key: 1,
+        label: "Etudiant",
+        montant: 150
       },
+      {
+        key: 2,
+        label: "Académique",
+        montant: 200
+      },
+      {
+        key: 3,
+        label: "Entreprise",
+        montant: 300
+      },
+    ];
+
+    const hebergements = [
+      {
+        key: 1,
+        label: 'Avec réservation',
+        montant: 150
+      },
+      {
+        key: 2,
+        label: 'Sans réservation',
+        montant: 0
+      },
+    ];
+    return {
+      hebergements,
+      inscriptions
+    }
+  },
+  computed: {
+    getSelectedInscription() {
+      return this.inscriptions.find((inscription) => parseFloat(inscription.montant) === parseFloat(this.formData.inscription));
     },
-  };
-  </script>
+    getSelectedHebergement() {
+      return this.hebergements.find((hebergement) => parseFloat(hebergement.montant) === parseFloat(this.formData.hebergement));
+    },
+    calculateTotalAmount() {
+      return parseFloat(this.formData.inscription) + parseFloat(this.formData.hebergement);
+    },
+  },
+};
+</script>
   
+<style scoped>
+  .summary{
+    margin-top: 2%;
+    margin-bottom: 2%;
+    border: 0.5px rgb(207, 207, 207) solid;
+    border-radius: 5px 5px;
+  }
+
+  .content-buttons{
+    margin-bottom: 2%;
+  }
+
+  .content-buttons button{
+    margin-left: 5px;
+    margin-right: 5px;
+  }
+</style>
